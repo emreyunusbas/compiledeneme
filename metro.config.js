46a6522c96a16225cdeaf9d0d04d74074e5e7769
Yunus Emre Baş <116023532+emreyunusbas@users.noreply.github.com>
2025-11-05 07:34:12 +0300

    Revert "[Compyle] Task cmhlgxhff0096r7imda95pjk1 updates for compiledeneme"

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for tRPC server files
-config.watchFolders = [__dirname];
-
-module.exports = config;
